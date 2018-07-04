04845b77e555d71d3a428dab518e9be4cd15b556
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-04 23:50:14 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,39 +1,18 @@
 class PigLatinizer
 
   def piglatinize(input_str)
-    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
-    puts x
-    x
-  end
-
-  private
-
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
-  end
-
-  def piglatinize_word(word)
-    # word starts with vowel
-    if !consonant?(word[0])
-      word = word + "w"
-    # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
+    def translate str
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u]
+    consonants = alpha - vowels
+  
+    if vowels.include?(str[0])
+      str + 'ay'
+    elsif consonants.include?(str[0]) && consonants.include?(str[1])
+      str[2..-1] + str[0..1] + 'ay'
+    elsif consonants.include?(str[0])
+      str[1..-1] + str[0] + 'ay'
     else
-      word = word.slice(1..-1) + word.slice(0)
+      str # return unchanged
     end
-    word << "ay"
   end
-
-  def piglatinize_sentence(sentence)
-    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
-  end
-
-
-
-
-end
