280a6e38fb8fe291c5956be4e9a98cddf4cd188b
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-04 23:52:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,18 +1,13 @@
 class PigLatinizer
 
-  def piglatinize(input_str)
-    alpha = ('a'..'z').to_a
-    vowels = %w[a e i o u]
-    consonants = alpha - vowels
-
-    if vowels.include?(str[0])
-      str + 'ay'
-    elsif consonants.include?(str[0]) && consonants.include?(str[1])
-      str[2..-1] + str[0..1] + 'ay'
-    elsif consonants.include?(str[0])
-      str[1..-1] + str[0] + 'ay'
-    else
-      str # return unchanged
+  def piglatinize(sent)
+        vowels = %w{a e i o u}
+        sent.gsub(/(\A|\s)\w+/) do |str|
+                str.strip!
+            while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+                str += str[0]
+                str = str[1..-1]
+            end
+            str  = ' ' + str + 'ay'
+        end.strip
     end
-  end
-end
